7883acdb3e363d33980d3354ee06c7f5fb98f0bf
JatinShimpi <shimpijatin184@gmail.com>
2025-04-05 02:39:37 +0530

    feat(bookings): add new routes for bookings and stays
    Introduce new routes to fetch bookings and stays after a specific date and to get today's stay activities. This enhances the functionality of the bookings module by providing more detailed data retrieval options.

--- src/routes/bookings.routes.js
+++ src/routes/bookings.routes.js
@@ -5,12 +5,18 @@ import {
   createBooking,
   deleteBooking,
   getAllBookings,
+  getBookingsAfterDate,
+  getStaysAfterDate,
+  getStaysTodayActivity
 } from "../controllers/bookings.controller.js";
 
 const router = Router();
 
 router.route("/create-booking").get(verifyJWT, createBooking);
 router.route("/get-bookings").get(verifyJWT, getAllBookings);
+router.route("/get-bookings-after-date").get(verifyJWT, getBookingsAfterDate);
+router.route("/get-stays-after-date").get(verifyJWT, getStaysAfterDate);
+router.route("/get-stays-today").get(verifyJWT, getStaysTodayActivity);
 router.route("/checkin-booking").post(verifyJWT, checkinBooking);
 router.route("/delete-booking").delete(verifyJWT, deleteBooking);
 
